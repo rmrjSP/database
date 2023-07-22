085751c5c19ae3206df4999c3b3507303b90aa8b
rmrjSP <92806752+rmrjSP@users.noreply.github.com>
2023-07-21 22:20:31 -0700

    insertRequestsWorking

--- controller.js
+++ controller.js
@@ -1,8 +1,15 @@
 var connection = require('./database');
 
 module.exports.addCompanyInfo = function(req, res){
-    let companyName = prompt("enter companny name");
-    let companyAddress = prompt("enter company address");
-
-    connection.query()
+    //var companyName = "ajaja";
+    //var companyAddress = "rhrh";
+    let sql = "insert into companyinfo(companyName, companyAddress) values ('rara','ehehe')";
+    connection.query(sql,function(err, results){
+        if (err) throw err;
+        res.send(results);
+    }) 
+    connection.query("select * from companyinfo",function(err, results){
+        if (err) throw err;
+        res.send(results);
+    }) 
 }
--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var express = require("express");
 var app = express();
 var connection = require('./database');
+const controller = require('./controller')
+
+/*var http = require('http'),
+    fs = require('fs');
 
 
+fs.readFile('./companyForm.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000); 
+});*/
 
-
-  app.get('/', function(req, res) {
-    
-    let sql = "Select * FROM companyInfo"
-    connection.query(sql, function(err, results){
-        if (err) throw err;
-        res.send(results);
-    })
-    
-});
-
+app.get('/', controller.addCompanyInfo);
 
 app.listen(3000, function(){
     console.log(' App Listening on port 3000');

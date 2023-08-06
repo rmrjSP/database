dc49a1485f9367422fed7c49c0c14184e9b499b8
rmrjSP <92806752+rmrjSP@users.noreply.github.com>
2023-08-05 21:45:57 -0700

    input1

--- controller.js
+++ controller.js
@@ -1,15 +1,36 @@
 var connection = require('./database');
-
+var companyname = "";
+var companyaddress = "";
 module.exports.addCompanyInfo = function(req, res){
-    //var companyName = "ajaja";
-    //var companyAddress = "rhrh";
-    let sql = "insert into companyinfo(companyName, companyAddress) values ('rara','ehehe')";
-    connection.query(sql,function(err, results){
-        if (err) throw err;
-        res.send(results);
-    }) 
-    connection.query("select * from companyinfo",function(err, results){
-        if (err) throw err;
-        res.send(results);
-    }) 
+    const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your name ? ", function(companyName) {
+    rl.question("Where do you live ? ", function(companyAddress) {
+        //console.log(`${companyName},  ${companyAddress}`);
+        companyname = `${companyName}`;
+        companyAddress = `${companyAddress}`;
+        rl.close();
+    });
+});
+ 
+ 
+let sql = "insert into companyinfo(companyName, companyAddress) values ('" + companyname + "','" + companyaddress + "')";
+connection.query(sql,function(err, results){
+    if (err) throw err;
+    res.send(results);
+}) 
+connection.query("select * from companyinfo",function(err, results){
+    if (err) throw err;
+    res.send(results);
+}) 
+
+  
+    rl.on("close", function() {
+        console.log("\nBYE BYE !!!");
+        process.exit(0);
+    });
 }
